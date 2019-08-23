5afb64475cce2353d376b731721e7568627d2bdf
Francisross Soares de Oliveira <franciscpd@gmail.com>
2019-08-23 08:37:19 -0300

    Tab navigator

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -121,14 +121,7 @@ class LoginPage extends StatelessWidget {
                               color: Colors.white,
                             ),
                           ),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HomePage(),
-                              ),
-                            );
-                          },
+                          onPressed: () {},
                         ),
                       ),
                     ],
